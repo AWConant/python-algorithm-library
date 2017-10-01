d81d38f367ea83eb6acec976bb47c6b80f3bd57a
Andrew <andrew.conant@comcast.net>
2017-10-01 16:27:23 -0700

    removed underscore from inversions and components module names

--- algorithm_library/array/tests/test_inversions.py
+++ algorithm_library/array/tests/test_inversions.py
@@ -1,4 +1,4 @@
-from .. import count_inversions
+from .. import inversions
 
 import pytest
 
@@ -11,6 +11,6 @@ import pytest
     ([2, 1, 3, 4, 5], 1),
 ])
 def test_general(xs, correct_inv_count):
-    in_order, inv_count = count_inversions.sort_count(xs)
+    in_order, inv_count = inversions.sort_count(xs)
     assert in_order == sorted(xs)
     assert correct_inv_count == inv_count
